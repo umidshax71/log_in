d02671d145a4b381ac99d4eb446e8726150035c0
umidshax71 <ismoilovumid609@gmail.com>
2023-07-29 11:05:46 +0300

    deploy

--- registrateon/settings.py
+++ registrateon/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@3wk_gc81_9ep*xb8t&w(zo7%^s-_6+^^*%_-85o+@mmu5lrzy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://registrations2006.pythonanywhere.com/']
 
 
 # Application definition
